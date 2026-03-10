1db86da1cf71a76bb004f680329d53aa8ecfb011
yunjindev <hunterweisenbach@me.com>
2026-03-10 09:56:07 -0400

    Working idea of timezone application

--- index.js
+++ index.js
@@ -1,8 +1,46 @@
-
+import dayjs from 'dayjs'
 import MicroModal from 'micromodal';
-
+import utc from 'dayjs/plugin/utc'
+import timezone from 'dayjs/plugin/timezone'
 MicroModal.init();
+dayjs.extend(utc)
+dayjs.extend(timezone)
 
-console.log("testing");
-
+//
 const currentTime = document.getElementById("currentTime");
+const timezoneSelect = document.getElementById("timezoneSelect");
+const currentDay = document.getElementById("currentDay");
+const currentTimezone = document.getElementById("currentTimezone");
+const selectedTimezone = timezoneSelect.value;
+
+let dateInterval = null;
+let timeInterval = null;
+
+setTimezone(selectedTimezone);
+
+function setTime(timezone) {
+    clearInterval(timeInterval);
+    timeInterval = setInterval(() => {
+        const time = dayjs().tz(timezone).format("HH:mm:ss");
+        currentTime.textContent = time;
+    },1000);
+}
+
+function setDate(timezone) {
+    clearInterval(dateInterval);
+    dateInterval = setInterval(()=> {
+        const day = dayjs().tz(timezone).format("MMMM D, YYYY");
+        currentDay.textContent = day;
+    },1000);
+}
+
+function setTimezone(timezone) {
+    currentTimezone.textContent = timezone;
+    setDate(timezone);
+    setTime(timezone);
+}
+
+timezoneSelect.addEventListener("change", () => {
+    const selectedTimezone = timezoneSelect.value;
+    setTimezone(selectedTimezone);
+});
